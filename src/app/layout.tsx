6b4a8a2acf9028e877aef363068b92b5747fccc6
Christian Mora <christianmoralopez@hotmail.com>
2024-11-19 16:06:29 -0500

    logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,10 +19,13 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/images/vision-svgrepo-com" />
-      </head>
+  <meta charSet="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="icon" type="image/svg+xml" href="/images/vision-svgrepo-com.svg?v=1" />
+  <link rel="icon" type="image/png" href="/images/vision-svgrepo-com.png" />
+  <link rel="icon" type="image/x-icon" href="/images/vision-svgrepo-com.ico" />
+</head>
+
       <body className={inter.className}>
         <Providers>
           {children}
